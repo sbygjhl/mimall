81850119d401f038cdf0a8e11c2d627e95d38cdd
lzc <2430357856@qq.com>
2020-07-10 00:00:52 +0800

    订单页面头部开发

---
+++ src/components/OrderHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="order-header">
+        <div class="container">
+            <div class="header-logo">
+                <a href="/#/index"></a>
+            </div>
+            <div class="title">
+                <h2>我的购物车</h2>
+                <slot name='tip'></slot>
+                
+            </div>
+            <div class="username">
+                <a href="javascript:;">Jack</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'oreder-header',
+    props:{
+        title:String
+    }
+}
+</script>
+<style lang="scss">
+@import './../assets/scss/mixin.scss';
+    .order-header{
+        .container{
+            @include flex();
+            padding: 30px 0;
+            .header-logo{
+                background: #FF6600;
+                width: 55px;
+                a{
+                    @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
+                }
+            }
+            .title{
+                width: 800px;
+                 height: 60px;
+                h2{
+                    display: inline-block;
+                    height: 55px;
+                    line-height: 55px;
+                    font-size: 28px;
+                }
+                span{
+                    display: inline-block;
+                    font-size: 14px;
+                    line-height: 55px;
+                    margin-left: 18px;
+                    color: #333333;
+                }
+                
+                
+            }
+            .username{
+                width: 80px;
+                height: 55px;
+                line-height: 55px;
+                font-size: 14px;
+            }
+        }
+        
+        
+    }
+</style>
--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="cart">
+        
+        <OrderHeader title="我得购物车">
+            <template v-slot:tip>
+                <span>
+                    温馨提示：产品是否购买成功，以最终下单为准11
+                </span>
+            </template>
+        </OrderHeader>
         <div class="container">
             cart
         </div>
     </div>
 </template>
+<script>
+import OrderHeader from './../components/OrderHeader';
+export default {
+    name:'cart',
+    components:{
+        OrderHeader
+    }
+}
+</script>
+<style lang='scss'>
+
+</style>
